5dc26aaff503b9bf2d52a211ab4c7d6fae815527
Dareal-slashergod <dblow2021@bklawtech.com>
2019-12-04 23:23:20 +0000

    RANDOM

--- js/script.js
+++ js/script.js
@@ -5,5 +5,10 @@
 //GLOBAL VARIABLES
 /* global $ */
 
-
+$(".login").click(function() {
+ var userchoice=$("#input").val();
+  var computerchoice=$("Computerchoice").val();
+ $("#userChoice").text(userchoice)
+ let num = Math.random(0.3);
+});
 
